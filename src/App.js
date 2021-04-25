9d0cca3a0fb99bee5872918c9303c1ebbf2123ca
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-04-25 12:34:29 +0300

    change path back

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from 'https://github.com/HunteRPVP/react_hw1_ex11/blob/main/src/logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
